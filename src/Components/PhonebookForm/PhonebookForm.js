ded2c87b07985b7051f35ceeb102e7a31bee32db
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-10-07 12:56:33 +0200

    phonebookForm refactored with hooks

--- src/Components/PhonebookForm/PhonebookForm.js
+++ src/Components/PhonebookForm/PhonebookForm.js
@@ -1,74 +1,78 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import style from "./PhonebookForm.module.css";
 import PropTypes from "prop-types";
 
-class PhonebookForm extends Component {
-  state = {
-    id: "",
-    name: "",
-    number: ""
+export default function PhonebookForm({ onSubmit }) {
+  // eslint-disable-next-line no-unused-vars
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleChange = evt => {
-    this.setState({ [evt.currentTarget.name]: evt.target.value });
+  const resetForm = () => {
+    setId("");
+    setName("");
+    setNumber("");
   };
 
-  resetForm = () => {
-    this.setState({
-      id: "",
-      name: "",
-      number: ""
-    });
-  };
-
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-
-    this.props.onSubmit(this.state.name, this.state.number);
-    this.resetForm();
+    onSubmit(name, number);
+    resetForm();
   };
 
-  render() {
-    return (
-      <form className={style.formWarpper} onSubmit={this.handleSubmit}>
-        <div className={style.formInputWarpper}>
-          <label className={style.lableName} htmlFor="name">
-            Name
-            <input
-              className={style.inputField}
-              type="text"
-              name="name"
-              value={this.state.name}
-              autoComplete="off"
-              onChange={this.handleChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
-          </label>
-        </div>
-        <div className={style.formInputWarpper}>
-          <label className={style.lableName} htmlFor="number">
-            Number
-            <input
-              className={style.inputField}
-              type="tel"
-              name="number"
-              value={this.state.number}
-              autoComplete="off"
-              onChange={this.handleChange}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-              required
-            />
-          </label>
-        </div>
-        <button className={style.formBtn} type="submit">
-          Add contact to phonebook
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={style.formWarpper} onSubmit={handleSubmit}>
+      <div className={style.formInputWarpper}>
+        <label className={style.lableName} htmlFor="name">
+          Name
+          <input
+            className={style.inputField}
+            type="text"
+            name="name"
+            value={name}
+            autoComplete="off"
+            onChange={handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+        </label>
+      </div>
+      <div className={style.formInputWarpper}>
+        <label className={style.lableName} htmlFor="number">
+          Number
+          <input
+            className={style.inputField}
+            type="tel"
+            name="number"
+            value={number}
+            autoComplete="off"
+            onChange={handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+          />
+        </label>
+      </div>
+      <button className={style.formBtn} type="submit">
+        Add contact to phonebook
+      </button>
+    </form>
+  );
 }
 
 PhonebookForm.defaultPops = {
@@ -80,5 +84,3 @@ PhonebookForm.defaultPops = {
 PhonebookForm.propTypes = {
   onSubmit: PropTypes.func.isRequired
 };
-
-export default PhonebookForm;
